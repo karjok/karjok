075a776bd428ec67ec58507cfc275269944cda9c
karjok <mulungd0l4r@gmail.com>
2021-11-01 22:05:48 +0700

    nambahin [cdba7ccbe8897dd79f2fd4952cb5c8a1, anjay, 38260461ce089c9d938b393f03748f0b, 20999926e3c698ccf02e1b1941d2f5a4]

--- updt.py
+++ updt.py
@@ -1,8 +1,8 @@
 import subprocess as sp
 
 def update(data):
-	if ":" in data:
-		data = list(set([x.split(":")[1].strip() for x in data.splitlines() if x != ""]))
+	if len(data.splitlines()) > 1:
+		data = list(set([x.split(":")[1].strip() for x in data.splitlines() if x != "" and ":" in x]))
 	else:
 		data = [data.strip()]
 	f = list(set([i.strip() for i in open("mydev.txt","r").readlines()]))
@@ -25,21 +25,26 @@ def update(data):
 				f.append(d)
 				n += 1
 				print(d,"data ditambahkan")
-	print(n, "data berhasil ditambahkan")
-	print("menulis file..")
-	with open("mydev.txt","w") as file:
-		for x in f:
-			file.write(x+"\n")
-		file.close()
-	print("done.")
-	print("update repository..")
-	sp.call(f"git add *;git commit -am 'nambahin {data}';git push -u https://github.com/karjok/karjok.git",shell=True,stdout=sp.PIPE)
-	print("done.")
+	if n != 0:
+		print(n, "data berhasil ditambahkan")
+		print("menulis file..")
+		with open("mydev.txt","w") as file:
+			for x in f:
+				file.write(x+"\n")
+			file.close()
+		print("done.")
+		print("update repository..")
+		sp.call(f"git add *;git commit -am 'nambahin {data}';git push -u https://github.com/karjok/karjok.git",shell=True,stdout=sp.PIPE)
+		print("done.")
+	else:
+		print("tidak ada yang perlu di update.")
 
 data = """
 [1/11 6.23 PM] Bg Ardi Customer: 20999926e3c698ccf02e1b1941d2f5a4
 [1/11 6.29 PM] Bg Ardi Customer: 38260461ce089c9d938b393f03748f0b
 [1/11 6.31 PM] Bg Ardi Customer: 38260461ce089c9d938b393f03748f0b
 [1/11 7.01 PM] Bg Ardi Customer: cdba7ccbe8897dd79f2fd4952cb5c8a1
+pplllll anjayy
+ps: anjay
 """
 update(data)
